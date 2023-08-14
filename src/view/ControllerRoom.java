180b23d48daf021c6382ecefbe7be1ae17d3ebda
danu <danujagreru@gmail.com>
2023-08-14 15:10:40 +0530

    basic classes

---
+++ src/view/ControllerRoom.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import view.NoticeBoardWindow;
+import view.SplitterWindow;
+
+/**
+ *
+ * @author danu
+ */
+public class ControllerRoom {
+    
+    NoticeBoardWindow nw;
+}
---
+++ src/view/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import view.ControllerRoom;
+import view.NoticeBoardWindow;
+
+/**
+ *
+ * @author danu
+ */
+public class Main {
+    public static void main(String[] args) {
+        NoticeBoardWindow nw = new NoticeBoardWindow();
+        
+        ControllerRoom cr = new ControllerRoom();
+        
+        
+    }
+}
